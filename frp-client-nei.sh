480d81fcde258335e17eb6531716d46ebd387a7f
Jinwen <toono@qq.com>
2018-08-28 17:07:20 +0800

    Update frp-client-nei.sh

--- frp-client-nei.sh
+++ frp-client-nei.sh
@@ -1,7 +1,9 @@
 apt update && apt install -y wget vim
 
-mkdir /frp-ssh https://raw.githubusercontent.com/ToonoW/frp-client/master/frpc-amd64
+mkdir /frp-ssh
+wget -P /frp-ssh/ https://raw.githubusercontent.com/ToonoW/frp-client/master/frpc-amd64
 wget -P /frp-ssh/ https://raw.githubusercontent.com/ToonoW/frp-client/master/frpc-ssh-nei.ini
+chmod 777 /frp-ssh/frpc-amd64
 
 nohup /frp-ssh/frpc-amd64 /frpc-ssh-nei.ini &
 
