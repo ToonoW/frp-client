1cf107974d172ff82b6007025984acd7495bae48
Jinwen <toono@qq.com>
2018-08-28 17:07:41 +0800

    Update frp-client-nei.sh

--- frp-client-nei.sh
+++ frp-client-nei.sh
@@ -5,6 +5,6 @@ wget -P /frp-ssh/ https://raw.githubusercontent.com/ToonoW/frp-client/master/frp
 wget -P /frp-ssh/ https://raw.githubusercontent.com/ToonoW/frp-client/master/frpc-ssh-nei.ini
 chmod 777 /frp-ssh/frpc-amd64
 
-nohup /frp-ssh/frpc-amd64 /frpc-ssh-nei.ini &
+nohup /frp-ssh/frpc-amd64 -c /frpc-ssh-nei.ini &
 
 
